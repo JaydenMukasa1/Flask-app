74e31412647872172abcdbf7d8a9d39abcf39e8f
JaydenMukasa1 <140836894+JaydenMukasa1@users.noreply.github.com>
2026-03-05 19:16:50 +0000

    Added redis code

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+r = redis.Redis(host='redis', port=6379)
 
 @app.route("/")
 def home():
     return "Welcome to my Flask app!"
 
+@app.route('/count')
+def count():
+    visits = r.incr('count')
+    return f'Visit count: {visits}'
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001)
